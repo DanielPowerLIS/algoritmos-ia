41af2b3723b49d70c5427f936ea3797e4da3a5f4
DanielPowerLIS <jdaniel070905@gmail.com>
2026-07-16 00:19:56 -0600

    algoritmo de breadth first search, terminado

---
+++ uninformed-searches/bfs.py
@@ -0,0 +1,22 @@
+from structures.queue import Queue
+
+def breadth_first_search(graph, start_node):
+    queue = Queue()
+    visited = set()
+    traversal= []
+
+    queue.enqueue(start_node)
+    visited.add(start_node)
+    while not queue.is_empty():
+        current = queue.dequeue()
+        current_node = graph.get_node(current)    
+        traversal.append(current_node.value)
+
+        for neighbor in current_node.neighbors:
+            if neighbor not in visited:
+                queue.enqueue(neighbor)
+                visited.add(neighbor)   
+
+    return traversal
+
+
